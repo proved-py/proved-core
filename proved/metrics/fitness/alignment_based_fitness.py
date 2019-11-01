fd590f9194daa9b813ab5721778e4313b972f926
Marco Pegoraro <mapegora@gmail.com>
2019-11-01 22:23:54 +0100

    Fixed formatting.

--- proved/metrics/fitness/alignment_based_fitness.py
+++ proved/metrics/fitness/alignment_based_fitness.py
@@ -7,6 +7,7 @@ def alignment_based_trace_fitness(u_trace, petri_net, initial_marking, final_mar
     alignment_lower_bound, alignment_upper_bound = alignment_bounds_su_trace(u_trace, petri_net, initial_marking, final_marking, parameters)
     return alignment_lower_bound['fitness'], alignment_upper_bound['fitness']
 
+
 # TODO: see proved.algorithms.conformance.alignments.alignment_bounds_su.alignment_bounds_su_log, needs to be smarter and receive a dictionary
 def alignment_based_log_fitness(u_log, petri_net, initial_marking, final_marking, parameters=None):
     log_alignments = list(zip(*alignment_bounds_su_log(u_log, petri_net, initial_marking, final_marking, parameters)))
