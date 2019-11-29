7d98fb6b715969cbbea8052f447f22160f27e2ff
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2019-11-29 16:09:38 +0100

    Implemented both versions of behavior graph.

--- proved/artifacts/behavior_graph/behavior_graph.py
+++ proved/artifacts/behavior_graph/behavior_graph.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 
 from networkx import DiGraph
+from networkx.algorithms.dag import transitive_reduction
 from pm4py.objects.log.util import xes
 
 import proved.xes_keys as xes_keys
 
 
-# TODO: clean code and remove the comments
 # TODO: absolutely needs a comparison function
 class BehaviorGraph(DiGraph):
 
     def __init__(self, trace, activity_key=xes.DEFAULT_NAME_KEY, timestamp_key=xes.DEFAULT_TIMESTAMP_KEY,
-                 u_timestamp_left=xes_keys.DEFAULT_U_TIMESTAMP_LEFT_KEY, u_timestamp_right=xes_keys.DEFAULT_U_TIMESTAMP_RIGHT_KEY,
+                 u_timestamp_left=xes_keys.DEFAULT_U_TIMESTAMP_LEFT_KEY,
+                 u_timestamp_right=xes_keys.DEFAULT_U_TIMESTAMP_RIGHT_KEY,
                  u_missing=xes_keys.DEFAULT_U_MISSING_KEY, u_activity_key=xes_keys.DEFAULT_U_NAME_KEY):
         DiGraph.__init__(self)
 
@@ -20,30 +21,16 @@ class BehaviorGraph(DiGraph):
         edges_list = []
         for i, event in enumerate(trace):
             if u_activity_key not in event:
-                # new_node = transition_system.TransitionSystem.State(str(i) + ': ' + trace[i][activity_key])
-                # new_node.data = (trace[i], [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + trace[i][activity_key], trace[i][activity_key])])
                 if u_missing not in event:
                     new_node = frozenset([event[activity_key]])
                 else:
                     new_node = frozenset([event[activity_key], None])
             else:
-                # new_node = transition_system.TransitionSystem.State(
-                #     str(i) + ': {' + ', '.join(list(trace[i][u_activity_key]['children'].keys())) + '}')
-                # new_node.data = (trace[i], [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + activity, activity) for activity in
-                #                              trace[i][u_activity_key]['children']])
                 if u_missing not in event:
                     new_node = frozenset(event[u_activity_key]['children'])
                 else:
                     new_node = frozenset(event[u_activity_key]['children'] + [None])
-            # if u_missing in event:
-                # new_node.data[1].append(petri.petrinet.PetriNet.Transition('t' + str(i) + '_silent', None))
-                # new_node.add(None)
-                # if u_activity_key not in trace[i]:
-                #     new_node.name = str(i) + ': {' + trace[i][activity_key] + ', ε}'
-                # else:
-                #     new_node.name = str(i) + ': {' + ', '.join(list(trace[i][u_activity_key]['children'].keys())) + ', ε}'
 
-            # ts.states.add(new_node)
             nodes_list.append(new_node)
 
             # Fill in the timestamps list
@@ -57,16 +44,10 @@ class BehaviorGraph(DiGraph):
         timestamps_list.sort()
 
         # Adding events 'Start' and 'End' in the list
-        # start = transition_system.TransitionSystem.State('start')
-        # start.data = (None, [petri.petrinet.PetriNet.Transition('start', None)])
         start = frozenset([None])
-        # ts.states.add(start)
         nodes_list.append(start)
         self.__root = start
-        # end = transition_system.TransitionSystem.State('end')
-        # end.data = (None, [petri.petrinet.PetriNet.Transition('end', None)])
         end = frozenset([None])
-        # ts.states.add(end)
         nodes_list.append(end)
 
         # Adding the nodes to the graph object
@@ -79,10 +60,8 @@ class BehaviorGraph(DiGraph):
             if timestamp1[2] != 'LEFT':
                 for timestamp2 in timestamps_list[i + 1:]:
                     if timestamp2[2] == 'LEFT':
-                        # utils.add_arc_from_to(repr(timestamp1[1]) + ' > ' + repr(timestamp2[1]), timestamp1[1], timestamp2[1], ts)
                         edges_list.append((timestamp1[1], timestamp2[1]))
                     if timestamp2[2] == 'CERTAIN':
-                        # utils.add_arc_from_to(repr(timestamp1[1]) + ' > ' + repr(timestamp2[1]), timestamp1[1], timestamp2[1], ts)
                         edges_list.append((timestamp1[1], timestamp2[1]))
                         break
                     if timestamp2[2] == 'RIGHT':
@@ -98,124 +77,72 @@ class BehaviorGraph(DiGraph):
     root = property(__get_root)
 
 
-class OldBehaviorGraph(DiGraph):
-
-    def __init__(self, trace, activity_key=xes.DEFAULT_NAME_KEY, timestamp_key=xes.DEFAULT_TIMESTAMP_KEY,
-                 u_timestamp_left=xes_keys.DEFAULT_U_TIMESTAMP_LEFT_KEY, u_timestamp_right=xes_keys.DEFAULT_U_TIMESTAMP_RIGHT_KEY,
-                 u_missing=xes_keys.DEFAULT_U_MISSING_KEY, u_activity_key=xes_keys.DEFAULT_U_NAME_KEY):
-        DiGraph.__init__(self)
-
-        timestamps_list = []
-        nodes_list = []
-        edges_list = []
-        for i, event in enumerate(trace):
-            if u_activity_key not in event:
-                # new_node = transition_system.TransitionSystem.State(str(i) + ': ' + trace[i][activity_key])
-                # new_node.data = (trace[i], [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + trace[i][activity_key], trace[i][activity_key])])
-                if u_missing not in event:
-                    new_node = frozenset([event[activity_key]])
-                else:
-                    new_node = frozenset([event[activity_key], None])
-            else:
-                # new_node = transition_system.TransitionSystem.State(
-                #     str(i) + ': {' + ', '.join(list(trace[i][u_activity_key]['children'].keys())) + '}')
-                # new_node.data = (trace[i], [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + activity, activity) for activity in
-                #                              trace[i][u_activity_key]['children']])
-                if u_missing not in event:
-                    new_node = frozenset(event[u_activity_key]['children'])
-                else:
-                    new_node = frozenset(event[u_activity_key]['children'] + [None])
-            # if u_missing in event:
-                # new_node.data[1].append(petri.petrinet.PetriNet.Transition('t' + str(i) + '_silent', None))
-                # new_node.add(None)
-                # if u_activity_key not in trace[i]:
-                #     new_node.name = str(i) + ': {' + trace[i][activity_key] + ', ε}'
-                # else:
-                #     new_node.name = str(i) + ': {' + ', '.join(list(trace[i][u_activity_key]['children'].keys())) + ', ε}'
-
-            # ts.states.add(new_node)
-            nodes_list.append(new_node)
-
-            # Fill in the timestamps list
-            if u_timestamp_left not in event:
-                timestamps_list.append((event[timestamp_key], new_node, 'CERTAIN'))
-            else:
-                timestamps_list.append((event[u_timestamp_left], new_node, 'LEFT'))
-                timestamps_list.append((event[u_timestamp_right], new_node, 'RIGHT'))
-
-        # Sort timestamps_list by first term of its elements
-        timestamps_list.sort()
-
-        # Adding events 'Start' and 'End' in the list
-        # start = transition_system.TransitionSystem.State('start')
-        # start.data = (None, [petri.petrinet.PetriNet.Transition('start', None)])
-        start = frozenset([None])
-        # ts.states.add(start)
-        nodes_list.append(start)
-        self.__root = start
-        # end = transition_system.TransitionSystem.State('end')
-        # end.data = (None, [petri.petrinet.PetriNet.Transition('end', None)])
-        end = frozenset([None])
-        # ts.states.add(end)
-        nodes_list.append(end)
-
-        # Adding the nodes to the graph object
-        self.add_nodes_from(nodes_list)
-
-        timestamps_list.insert(0, (datetime.min, start, 'CERTAIN'))
-        timestamps_list.append((datetime.max, end, 'CERTAIN'))
-
-        for i, timestamp1 in enumerate(timestamps_list):
-            if timestamp1[2] != 'LEFT':
-                for timestamp2 in timestamps_list[i + 1:]:
-                    if timestamp2[2] == 'LEFT':
-                        # utils.add_arc_from_to(repr(timestamp1[1]) + ' > ' + repr(timestamp2[1]), timestamp1[1], timestamp2[1], ts)
-                        edges_list.append((timestamp1[1], timestamp2[1]))
-                    if timestamp2[2] == 'CERTAIN':
-                        # utils.add_arc_from_to(repr(timestamp1[1]) + ' > ' + repr(timestamp2[1]), timestamp1[1], timestamp2[1], ts)
-                        edges_list.append((timestamp1[1], timestamp2[1]))
-                        break
-                    if timestamp2[2] == 'RIGHT':
-                        if (timestamp1[1], timestamp2[1]) in edges_list:
-                            break
-
-        # Adding the edges to the graph object
-        self.add_edges_from(edges_list)
-
-    def __get_root(self):
-        return self.__root
-
-    root = property(__get_root)
-
-
-def construct_behavior_graph_transitive_reduction(trace, activity_key=xes.DEFAULT_NAME_KEY, u_missing=xes_keys.DEFAULT_U_MISSING_KEY,
-                                                  u_activity_key=xes_keys.DEFAULT_U_NAME_KEY):
-    ts = transition_system.TransitionSystem()
-    start = transition_system.TransitionSystem.State('start')
-    start.data = (None, [petri.petrinet.PetriNet.Transition('start', None)])
-    ts.states.add(start)
-    end = transition_system.TransitionSystem.State('end')
-    end.data = (None, [petri.petrinet.PetriNet.Transition('end', None)])
-    ts.states.add(end)
-    for i, event in enumerate(trace):
-        if u_activity_key not in event:
-            new_state = transition_system.TransitionSystem.State(str(i) + ': ' + event[activity_key])
-            new_state.data = (event, [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + event[activity_key], event[activity_key])])
+def ordered(event1, event2, timestamp_key=xes.DEFAULT_TIMESTAMP_KEY,
+            u_timestamp_left=xes_keys.DEFAULT_U_TIMESTAMP_LEFT_KEY,
+            u_timestamp_right=xes_keys.DEFAULT_U_TIMESTAMP_RIGHT_KEY):
+    if u_timestamp_right in event1:
+        if u_timestamp_left in event2:
+            return event1[u_timestamp_right] < event2[u_timestamp_left]
         else:
-            new_state = transition_system.TransitionSystem.State(str(i) + ': {' + ', '.join(list(event[u_activity_key]['children'].keys())) + '}')
-            new_state.data = (
-                event,
-                [petri.petrinet.PetriNet.Transition('t' + str(i) + '_' + activity, activity) for activity in event[u_activity_key]['children']])
-        if u_missing in event:
-            new_state.data[1].append(petri.petrinet.PetriNet.Transition('t' + str(i) + '_silent', None))
-            new_state.name = new_state.name + '_ε'
+            return event1[u_timestamp_right] < event2[timestamp_key]
+    else:
+        if u_timestamp_left in event2:
+            return event1[timestamp_key] < event2[u_timestamp_left]
+        else:
+            return event1[timestamp_key] < event2[timestamp_key]
 
-        utils.add_arc_from_to('start > ' + repr(new_state), start, new_state, ts)
-        utils.add_arc_from_to(repr(new_state) + ' > end', new_state, end, ts)
-        for state in ts.states:
-            if state.name is not 'start' and state.name is not 'end' and ordered(state.data[0], event):
-                utils.add_arc_from_to(repr(state) + ' > ' + repr(new_state), state, new_state, ts)
-        ts.states.add(new_state)
 
-    utils.transitive_reduction(ts)
-    return ts
+class TRBehaviorGraph(DiGraph):
+
+    def __init__(self, trace, activity_key=xes.DEFAULT_NAME_KEY, u_missing=xes_keys.DEFAULT_U_MISSING_KEY,
+                 u_activity_key=xes_keys.DEFAULT_U_NAME_KEY):
+        DiGraph.__init__(self)
+
+        bg = DiGraph()
+
+        start = frozenset([None])
+        bg.add_node(start)
+        bg.__root = start
+        end = frozenset([None])
+        bg.add_node(end)
+
+        nodes_list = []
+        edges_list = []
+        event_node_map = {}
+
+        for i, event in enumerate(trace):
+            if u_activity_key not in event:
+                if u_missing not in event:
+                    new_node = frozenset([event[activity_key]])
+                else:
+                    new_node = frozenset([event[activity_key], None])
+            else:
+                if u_missing not in event:
+                    new_node = frozenset(event[u_activity_key]['children'])
+                else:
+                    new_node = frozenset(event[u_activity_key]['children'] + [None])
+
+            nodes_list.append(new_node)
+
+            edges_list.append((start, new_node))
+            edges_list.append((new_node, end))
+            event_node_map[i] = new_node
+
+        for i, event1 in enumerate(trace):
+            for j, event2 in enumerate(trace):
+                if ordered(event1, event2):
+                    edges_list.append((event1, event2))
+
+        bg.add_nodes_from(nodes_list)
+        bg.add_edges_from(edges_list)
+
+        bg = transitive_reduction(bg)
+
+        self.add_nodes_from(bg.nodes)
+        self.__root = start
+        self.add_edges_from(bg.edges)
+
+    def __get_root(self):
+        return self.__root
+
+    root = property(__get_root)
