3bf559dca95c487b2fd22bafe4a61783935af0b2
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2019-12-11 12:43:34 +0100

    Added filenames in the test.

--- exp_bis.py
+++ exp_bis.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     # lengths = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]
     lengths = [100, 150, 200, 250, 300, 350, 400, 450, 500]
     reallife_probs = [0, .4, .8]
-    reallife_logs = ['', '', '']
+    reallife_logs = ['BPI_Challenge_2012.xes', 'Help_Desk_event_log.xes', 'Road_Traffic_Fine_Management_Process.xes']
 
     random.seed(123456)
 
