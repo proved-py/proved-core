e2c4783f1fa7613d33b28fda0451ddf26c04608f
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2019-12-11 12:39:03 +0100

    Added real-life logs experiment.

--- exp_bis.py
+++ exp_bis.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from datetime import timedelta
 
 from pm4py.objects.log.log import Trace, Event
+from pm4py.objects.log.importer.xes import factory as xes_import_factory
 
 from proved.artifacts.behavior_graph import behavior_graph
 
@@ -27,6 +28,23 @@ def create_log(numtraces, lentraces, p_u_time):
                     event["u:time:timestamp_left"] = event['time:timestamp'] - timevariation
                     event["u:time:timestamp_right"] = event['time:timestamp'] + timevariation
     return log
+
+
+def introduce_uncertainty(log, p_u_time):
+    timevariation = timedelta(seconds=1)
+    for trace in log:
+        if len(trace) > 1:
+            for i, event in enumerate(trace):
+                if random.random() < p_u_time:
+                    if i == 0:
+                        event["u:time:timestamp_left"] = event['time:timestamp']
+                        event["u:time:timestamp_right"] = trace[i + 1]['time:timestamp'] + timevariation
+                    elif i == len(trace) - 1:
+                        event["u:time:timestamp_left"] = trace[i - 1]['time:timestamp'] - timevariation
+                        event["u:time:timestamp_right"] = event['time:timestamp']
+                    else:
+                        event["u:time:timestamp_left"] = trace[i - 1]['time:timestamp'] - timevariation
+                        event["u:time:timestamp_right"] = trace[i + 1]['time:timestamp'] + timevariation
 
 
 fixed_prob = .4
@@ -85,11 +103,33 @@ def length_experiment(lengths):
     return naive_times, improved_times
 
 
+def reallife_experiments(logfile, probs):
+    naive_times = []
+    improved_times = []
+    for p_u_time in probs:
+        log = xes_import_factory.apply(logfile)
+        introduce_uncertainty(log, p_u_time)
+        a = time.process_time()
+        for trace in log:
+            bg = behavior_graph.TRBehaviorGraph(trace)
+        naive_times.append(time.process_time() - a)
+        a = time.process_time()
+        for trace in log:
+            bg = behavior_graph.BehaviorGraph(trace)
+        improved_times.append(time.process_time() - a)
+
+    return naive_times, improved_times
+
+
 if __name__ == '__main__':
     probs = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
-    nstraces = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
+    nstraces = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500,
+                9000, 9500, 10000]
     # lengths = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-    lengths = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]
+    # lengths = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300]
+    lengths = [100, 150, 200, 250, 300, 350, 400, 450, 500]
+    reallife_probs = [0, .4, .8]
+    reallife_logs = ['', '', '']
 
     random.seed(123456)
 
@@ -119,14 +159,31 @@ if __name__ == '__main__':
     #     for line in ntraces_results:
     #         csvwriter.writerow(line[1])
 
-    length_results = [length_experiment(lengths) for i in range(ntests)]
-    with open('lengths_results_naive.csv', 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
-        csvwriter.writerow(lengths)
-        for line in length_results:
-            csvwriter.writerow(line[0])
-    with open('lengths_results_improved.csv', 'w') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
-        csvwriter.writerow(lengths)
-        for line in length_results:
-            csvwriter.writerow(line[1])
+    ############################################################################
+    ############################################################################
+    ############################################################################
+    # length_results = [length_experiment(lengths) for i in range(ntests)]
+    # with open('lengths_results_naive.csv', 'w') as csvfile:
+    #     csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+    #     csvwriter.writerow(lengths)
+    #     for line in length_results:
+    #         csvwriter.writerow(line[0])
+    # with open('lengths_results_improved.csv', 'w') as csvfile:
+    #     csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+    #     csvwriter.writerow(lengths)
+    #     for line in length_results:
+    #         csvwriter.writerow(line[1])
+    #
+    for logfile in reallife_logs:
+        reallife_results = [reallife_experiments(logfile, reallife_probs) for i in range(ntests)]
+        with open(logfile + '_results_naive.csv', 'w') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+            csvwriter.writerow(reallife_probs)
+            for line in reallife_results:
+                csvwriter.writerow(line[0])
+        with open(logfile + '_results_improved.csv', 'w') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
+            csvwriter.writerow(reallife_probs)
+            for line in reallife_results:
+                csvwriter.writerow(line[1])
+
