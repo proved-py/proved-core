53aa6edd658d42675c5de6d1372d9a7580c7ef62
Marco Pegoraro <marcopegoraro@users.noreply.github.com>
2020-02-23 16:32:11 +0100

    Update Efficient_Construction_of_Behavior_Graphs_for_Uncertain_Event_Data.py
    Added links to data files.

--- experiments/Efficient_Construction_of_Behavior_Graphs_for_Uncertain_Event_Data.py
+++ experiments/Efficient_Construction_of_Behavior_Graphs_for_Uncertain_Event_Data.py
@@ -1,6 +1,11 @@
 """
 Experiments for the paper
     Pegoraro, Marco, Merih Seran Uysal, and Wil MP van der Aalst. "Efficient Construction of Behavior Graphs for Uncertain Event Data"
+Note: Needs additional data files to run:
+    * BPI_Challenge_2012.xes: from https://data.4tu.nl/repository/uuid:3926db30-f712-4394-aebc-75976070e91f
+    * Help_Desk_event_log.xes: from https://data.4tu.nl/repository/uuid:0c60edf1-6f83-4e75-9367-4c63b3e9d5bb
+    * Road_Traffic_Fine_Management_Process.xes: from https://data.4tu.nl/repository/uuid:270fd440-1057-4fb9-89a9-b699b47990f5
+
 """
 
 import os.path
