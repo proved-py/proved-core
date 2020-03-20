93e7c2f382ec2c88b034a8e710f3834c27e79027
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-03-20 17:18:52 +0100

    Renamed parameter.

--- proved/simulation/bewilderer/indeterminate_events.py
+++ proved/simulation/bewilderer/indeterminate_events.py
@@ -3,28 +3,28 @@ from random import random
 import proved.xes_keys as xes_keys
 
 
-def add_indeterminate_events_to_log(log, p, u_missing=xes_keys.DEFAULT_U_MISSING_KEY):
+def add_indeterminate_events_to_log(log, p, u_missing_key=xes_keys.DEFAULT_U_MISSING_KEY):
     """
     Turns events in an event log into indeterminate events with a certain probability.
 
     :param log: the event log
     :param p: the probability of indeterminate events
-    :param u_missing: the xes key for indeterminate events
+    :param u_missing_key: the xes key for indeterminate events
     :return:
     """
     for trace in log:
-        add_indeterminate_events_to_trace(trace, p, u_missing)
+        add_indeterminate_events_to_trace(trace, p, u_missing_key)
 
 
-def add_indeterminate_events_to_trace(trace, p, u_missing=xes_keys.DEFAULT_U_MISSING_KEY):
+def add_indeterminate_events_to_trace(trace, p, u_missing_key=xes_keys.DEFAULT_U_MISSING_KEY):
     """
     Turns events in an trace into indeterminate events with a certain probability.
 
     :param trace: the trace
     :param p: the probability of indeterminate events
-    :param u_missing: the xes key for indeterminate events
+    :param u_missing_key: the xes key for indeterminate events
     :return:
     """
     for event in trace:
         if random() < p:
-            event[u_missing] = 1
+            event[u_missing_key] = 1
