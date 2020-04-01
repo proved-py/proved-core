c86ae7b8fddc71fc1f63e267ec07785939999097
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-04-01 19:09:17 +0200

    Completed the simulation package.

--- proved/simulation/bewilderer/timestamps.py
+++ proved/simulation/bewilderer/timestamps.py
@@ -1,5 +1,7 @@
 from random import random
+from copy import copy
 
+import numpy as np
 from pm4py.objects.log.util.xes import DEFAULT_TIMESTAMP_KEY
 
 from proved.xes_keys import DEFAULT_U_TIMESTAMP_MIN_KEY, DEFAULT_U_TIMESTAMP_MAX_KEY
@@ -41,12 +43,32 @@ def add_uncertain_timestamp_to_trace_relative(trace, p_left, p_right, max_overla
     """
 
     if p_left > 0 or p_right > 0:
-        for i in range(trace):
+        for i in range(len(trace)):
             steps_left = 0
             steps_right = 0
             while random() < p_left and steps_left < min(max_overlap_left, i):
                 steps_left += 1
             while random() < p_right and steps_right < min(max_overlap_right, len(trace) - i - 1):
                 steps_right += 1
-            trace[i][u_timestamp_min_key] = trace[i - steps_left][timestamp_key]
-            trace[i][u_timestamp_max_key] = trace[i + steps_right][timestamp_key]
+            # (Partially) supports events that already have uncertainty on timestamps
+            # This might cause problems on events for which 'u_timestamp_min_key' <= 'timestamp_key' <= 'u_timestamp_max_key'
+            if u_timestamp_max_key in trace[i - steps_left]:
+                if u_timestamp_min_key in trace[i]:
+                    trace[i][u_timestamp_min_key] = copy(min(trace[i][u_timestamp_min_key], trace[i - steps_left][u_timestamp_max_key]))
+                else:
+                    trace[i][u_timestamp_min_key] = copy(min(trace[i][timestamp_key], trace[i - steps_left][u_timestamp_max_key]))
+            else:
+                if u_timestamp_min_key in trace[i]:
+                    trace[i][u_timestamp_min_key] = copy(min(trace[i][u_timestamp_min_key], trace[i - steps_left][timestamp_key]))
+                else:
+                    trace[i][u_timestamp_min_key] = copy(min(trace[i][timestamp_key], trace[i - steps_left][timestamp_key]))
+            if u_timestamp_min_key in trace[i + steps_right]:
+                if u_timestamp_max_key in trace[i]:
+                    trace[i][u_timestamp_max_key] = copy(max(trace[i][u_timestamp_max_key], trace[i + steps_right][u_timestamp_min_key]))
+                else:
+                    trace[i][u_timestamp_max_key] = copy(max(trace[i][timestamp_key], trace[i + steps_right][u_timestamp_min_key]))
+            else:
+                if u_timestamp_max_key in trace[i]:
+                    trace[i][u_timestamp_max_key] = copy(max(trace[i][u_timestamp_max_key], trace[i + steps_right][timestamp_key]))
+                else:
+                    trace[i][u_timestamp_max_key] = copy(max(trace[i][timestamp_key], trace[i + steps_right][timestamp_key]))
