b9b9f73a01a19cca1bc6b6a30d1d1a61cc40c650
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-08-05 16:34:15 +0200

    Completed compact representation for uncertain logs.

--- proved/artifacts/uncertain_log/uncertain_log.py
+++ proved/artifacts/uncertain_log/uncertain_log.py
@@ -14,6 +14,19 @@ class UncertainLog(EventLog):
 
     # TODO: method that given an uncertain log creates the behavior graphs and a mapping between uncertain traces and behavior graphs
     def create_behavior_graphs(self):
-        pass
+        # TODO: this still suffers from the bug of the behavior graph creation: timestamps must not coincide
+        # TODO: test this
+        for trace in self:
+            nodes_list = behavior_graph.create_nodes_tuples(trace)
+            if nodes_list not in self.behavior_graphs:
+                self.behavior_graphs[nodes_list] = (behavior_graph.BehaviorGraph(trace), [])
+            self.behavior_graphs[nodes_list][1].append(trace)
+
+    def create_named_variant_tuple(self):
+        # TODO: test this
+        if self.behavior_graphs is not {}:
+            variant_list = [(len(variant[1]), variant[0], variant[1]) for variant in self.behavior_graphs.items()]
+            variant_list.sort()
+            return tuple(variant_list)
 
     behavior_graphs = property(__get_behavior_graphs)
