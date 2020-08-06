3b70e46e6481d8d59da13af9516750d9ba84c330
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-08-06 11:17:22 +0200

    Completed and tested the compact uncertain log representation.

--- proved/artifacts/uncertain_log/uncertain_log.py
+++ proved/artifacts/uncertain_log/uncertain_log.py
@@ -5,9 +5,13 @@ from proved.artifacts.behavior_graph import behavior_graph
 
 class UncertainLog(EventLog):
 
-    def __init__(self):
-        EventLog.__init__(self)
+    def __init__(self, log=None):
         self.__behavior_graphs = dict()
+        if log is not None:
+            EventLog.__init__(self, log)
+            self.create_behavior_graphs()
+        else:
+            EventLog.__init__(self)
 
     def __get_behavior_graphs(self):
         return self.__behavior_graphs
@@ -15,18 +19,16 @@ class UncertainLog(EventLog):
     # TODO: method that given an uncertain log creates the behavior graphs and a mapping between uncertain traces and behavior graphs
     def create_behavior_graphs(self):
         # TODO: this still suffers from the bug of the behavior graph creation: timestamps must not coincide
-        # TODO: test this
         for trace in self:
             nodes_list = behavior_graph.create_nodes_tuples(trace)
             if nodes_list not in self.behavior_graphs:
                 self.behavior_graphs[nodes_list] = (behavior_graph.BehaviorGraph(trace), [])
             self.behavior_graphs[nodes_list][1].append(trace)
 
-    def create_named_variant_tuple(self):
-        # TODO: test this
+    def create_sorted_variant_tuple(self):
         if self.behavior_graphs is not {}:
-            variant_list = [(len(variant[1]), variant[0], variant[1]) for variant in self.behavior_graphs.items()]
-            variant_list.sort()
+            variant_list = [(len(variant[1]), i, variant[0], variant[1]) for i, (nodes_list, variant) in enumerate(self.behavior_graphs.items())]
+            variant_list.sort(reverse=True)
             return tuple(variant_list)
 
     behavior_graphs = property(__get_behavior_graphs)
