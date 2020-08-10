83908ec2f0cb3e9db0e9cdbf0ece865affcba8e7
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-08-10 13:56:52 +0200

    Refined the data structured in the uncertain log object.

--- proved/artifacts/uncertain_log/uncertain_log.py
+++ proved/artifacts/uncertain_log/uncertain_log.py
@@ -6,29 +6,35 @@ from proved.artifacts.behavior_graph import behavior_graph
 class UncertainLog(EventLog):
 
     def __init__(self, log=None):
-        self.__behavior_graphs = dict()
+        self.__variants = tuple()
+        self.__behavior_graphs_map = dict()
         if log is not None:
             EventLog.__init__(self, log)
             self.create_behavior_graphs()
         else:
             EventLog.__init__(self)
 
-    def __get_behavior_graphs(self):
-        return self.__behavior_graphs
+    def __get_variants(self):
+        return self.__variants
+
+    def __get_behavior_graphs_map(self):
+        return self.__behavior_graphs_map
 
     # TODO: method that given an uncertain log creates the behavior graphs and a mapping between uncertain traces and behavior graphs
     def create_behavior_graphs(self):
         # TODO: this still suffers from the bug of the behavior graph creation: timestamps must not coincide
         for trace in self:
-            nodes_list = behavior_graph.create_nodes_tuples(trace)
-            if nodes_list not in self.behavior_graphs:
-                self.behavior_graphs[nodes_list] = (behavior_graph.BehaviorGraph(trace), [])
-            self.behavior_graphs[nodes_list][1].append(trace)
-
-    def create_sorted_variant_tuple(self):
-        if self.behavior_graphs is not {}:
-            variant_list = [(len(variant[1]), i, variant[0], variant[1]) for i, (nodes_list, variant) in enumerate(self.behavior_graphs.items())]
+            nodes_tuple = behavior_graph.create_nodes_tuples(trace)
+            if nodes_tuple not in self.behavior_graphs_map:
+                self.behavior_graphs_map[nodes_tuple] = (behavior_graph.BehaviorGraph(trace), [])
+            self.behavior_graphs_map[nodes_tuple][1].append(trace)
+        if self.behavior_graphs_map is not {}:
+            variant_list = [(len(traces_list), nodes_list) for nodes_list, (_, traces_list) in self.behavior_graphs_map.items()]
             variant_list.sort(reverse=True)
-            return tuple(variant_list)
+            self.__variants = tuple((i, variant_length, nodes_tuple) for i, (variant_length, nodes_tuple) in enumerate(variant_list))
 
-    behavior_graphs = property(__get_behavior_graphs)
+    def get_behavior_graph(self, trace):
+        return self.behavior_graphs_map[behavior_graph.create_nodes_tuples(trace)]
+
+    variants = property(__get_variants)
+    behavior_graphs_map = property(__get_behavior_graphs_map)
