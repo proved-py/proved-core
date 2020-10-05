15f7397eb8225524b38b122bc844ebca3791cd1b
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2020-10-05 14:24:10 +0200

    Separated utility functions in dedicated file.

---
+++ experiments/utils.py
@@ -0,0 +1,212 @@
+import datetime
+import time
+from copy import copy
+from random import choice
+
+from lxml import etree
+from pm4py.objects import petri
+from pm4py.objects.log import log as log_instance
+from pm4py.objects.log.util import xes as xes_key
+from pm4py.objects.petri.common import final_marking
+from pm4py.objects.petri import semantics
+from pm4py.objects.random_variables.random_variable import RandomVariable
+
+
+# Fixes on functions of this version of PM4Py library
+
+
+def import_net(input_file_path, return_stochastic_information=False, parameters=None):
+    """
+    Import a Petri net from a PNML file
+
+    Parameters
+    ----------
+    input_file_path
+        Input file path
+    return_stochastic_information
+        Enables return of stochastic information if found in the PNML
+    parameters
+        Other parameters of the algorithm
+    """
+    if parameters is None:
+        parameters = {}
+
+    tree = etree.parse(input_file_path)
+    root = tree.getroot()
+
+    net = petri.petrinet.PetriNet('imported_' + str(time.time()))
+    marking = petri.petrinet.Marking()
+    fmarking = petri.petrinet.Marking()
+
+    nett = None
+    page = None
+    finalmarkings = None
+
+    stochastic_information = {}
+
+    for child in root:
+        nett = child
+
+    places_dict = {}
+    trans_dict = {}
+
+    if nett is not None:
+        for child in nett:
+            if "page" in child.tag:
+                page = child
+            if "finalmarkings" in child.tag:
+                finalmarkings = child
+
+    if page is None:
+        page = nett
+
+    if page is not None:
+        for child in page:
+            if "place" in child.tag:
+                place_id = child.get("id")
+                place_name = place_id
+                number = 0
+                for child2 in child:
+                    if "name" in child2.tag:
+                        for child3 in child2:
+                            if child3.text:
+                                place_name = child3.text
+                    if "initialMarking" in child2.tag:
+                        for child3 in child2:
+                            if "text" in child3.tag:
+                                number = int(child3.text)
+                places_dict[place_id] = petri.petrinet.PetriNet.Place(place_id)
+                net.places.add(places_dict[place_id])
+                if number > 0:
+                    marking[places_dict[place_id]] = number
+                del place_name
+
+    if page is not None:
+        for child in page:
+            if "transition" in child.tag:
+                trans_name = child.get("id")
+                trans_label = trans_name
+                trans_visible = True
+
+                random_variable = None
+
+                for child2 in child:
+                    if "name" in child2.tag:
+                        for child3 in child2:
+                            if child3.text:
+                                if trans_label == trans_name:
+                                    trans_label = child3.text
+                    if "toolspecific" in child2.tag:
+                        tool = child2.get("tool")
+                        if "ProM" in tool:
+                            activity = child2.get("activity")
+                            if "invisible" in activity:
+                                trans_visible = False
+                        elif "StochasticPetriNet" in tool:
+                            distribution_type = None
+                            distribution_parameters = None
+                            priority = None
+                            weight = None
+
+                            for child3 in child2:
+                                key = child3.get("key")
+                                value = child3.text
+
+                                if key == "distributionType":
+                                    distribution_type = value
+                                elif key == "distributionParameters":
+                                    distribution_parameters = value
+                                elif key == "priority":
+                                    priority = int(value)
+                                elif key == "weight":
+                                    weight = float(value)
+
+                            if return_stochastic_information:
+                                random_variable = RandomVariable()
+                                random_variable.read_from_string(distribution_type, distribution_parameters)
+                                random_variable.set_priority(priority)
+                                random_variable.set_weight(weight)
+                if not trans_visible:
+                    trans_label = None
+
+                trans_dict[trans_name] = petri.petrinet.PetriNet.Transition(trans_name, trans_label)
+                net.transitions.add(trans_dict[trans_name])
+
+                if random_variable is not None:
+                    stochastic_information[trans_dict[trans_name]] = random_variable
+
+    if page is not None:
+        for child in page:
+            if "arc" in child.tag:
+                arc_source = child.get("source")
+                arc_target = child.get("target")
+
+                if arc_source in places_dict and arc_target in trans_dict:
+                    petri.utils.add_arc_from_to(places_dict[arc_source], trans_dict[arc_target], net)
+                elif arc_target in places_dict and arc_source in trans_dict:
+                    petri.utils.add_arc_from_to(trans_dict[arc_source], places_dict[arc_target], net)
+
+    if finalmarkings is not None:
+        for child in finalmarkings:
+            for child2 in child:
+                place_id = child2.get("idref")
+                for child3 in child2:
+                    if "text" in child3.tag:
+                        number = int(child3.text)
+                        if number > 0:
+                            fmarking[places_dict[place_id]] = number
+
+    # generate the final marking in the case has not been found
+    if len(fmarking) == 0:
+        fmarking = final_marking.discover_final_marking(net)
+
+    if return_stochastic_information and len(list(stochastic_information.keys())) > 0:
+        return net, marking, fmarking, stochastic_information
+
+    return net, marking, fmarking
+
+
+def apply_playout(net, initial_marking, final_marking, no_traces=100, max_trace_length=100, trace_key=xes_key.DEFAULT_TRACEID_KEY, activity_key=xes_key.DEFAULT_NAME_KEY, timestamp_key=xes_key.DEFAULT_TIMESTAMP_KEY):
+    """
+    Do the playout of a Petrinet generating a log
+
+    Parameters
+    ----------
+    net
+        Petri net to play-out
+    initial_marking
+        Initial marking of the Petri net
+    final_marking
+        Final marking of the Petri net
+    no_traces
+        Number of traces to generate
+    max_trace_length
+        Maximum number of events per trace (do break)
+    """
+    # assigns to each event an increased timestamp from 1970
+    curr_timestamp = 10000000
+    log = log_instance.EventLog()
+    for i in range(no_traces):
+        trace = log_instance.Trace()
+        trace.attributes[trace_key] = str(i)
+        marking = copy(initial_marking)
+        while len(trace) < max_trace_length:
+            if not semantics.enabled_transitions(net, marking):  # supports nets with possible deadlocks
+                break
+            all_enabled_trans = semantics.enabled_transitions(net, marking)
+            if marking == final_marking:
+                trans = choice(tuple(all_enabled_trans.union({None})))
+            else:
+                trans = choice(tuple(all_enabled_trans))
+            if trans is None:
+                break
+            if trans.label is not None:
+                event = log_instance.Event()
+                event[activity_key] = trans.label
+                event[timestamp_key] = datetime.datetime.fromtimestamp(curr_timestamp)
+                trace.append(event)
+                # increases by 1 second
+                curr_timestamp += 1
+            marking = semantics.execute(trans, net, marking)
+        log.append(trace)
+    return log
