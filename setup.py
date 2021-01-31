34eb811743b46729d2740c9baaa1b26904ac886a
Marco Pegoraro <pegoraro@pads.rwth-aachen.de>
2021-01-31 16:11:06 +0100

    Advanced subversion number.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/marcopegoraro/proved',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     python_requires='>=3.6',
 )
